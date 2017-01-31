e3ac5a97a8bc0dd0ca7cf38b61fbecb212258fe1
ckabuloglu <ckabuloglu@gmail.com>
2017-01-31 17:17:06 -0500

    Linked List Questions

--- checkPalindromeLinkedList.py
+++ checkPalindromeLinkedList.py
@@ -24,6 +24,36 @@ class Node:
             cur = cur.next
         return ''.join(st)
 
-# Algorithm (will be completed)
-def checkPalindrome():
-    return null
+# Algorithm
+def isPalindrome(head):
+    fast = head
+    slow = head
+    stack = []
+
+    while fast and fast.next:
+        stack.append(slow.data)
+        slow = slow.next
+        fast = fast.next.next
+
+    if fast: slow = slow.next
+
+    while stack:
+        if stack.pop() == slow.data: slow = slow.next
+        else: return False
+        
+    return True
+
+l1 = [0,1,2,3,3,2,1,0]
+l2 = [0,1,2,3,2,1,0]
+l3 = [0,1,2,3,2,1,1]
+l4 = [1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1]
+
+llist = Node(l4[0])
+head = llist
+
+for i in l4[1:]:
+    llist.next = Node(i)
+    llist = llist.next
+
+print head
+print isPalindrome(head)  
