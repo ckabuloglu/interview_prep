1afc3e2e2a5e77c27578101189e5b47065432316
ckabuloglu <ckabuloglu@gmail.com>
2017-02-02 17:54:57 -0500

    Trees

--- minimalTree.py
+++ minimalTree.py
@@ -7,6 +7,16 @@ class TreeNode():
 
 
 def makeTree(arr):
-    
-    return TreeNode
-    
+    if len(arr) == 1:
+        return TreeNode(arr[0])
+    elif len(arr) == 2:
+        return TreeNode(arr[1], TreeNode(arr[0]), None)
+    else:
+        ind = len(arr) / 2
+        return TreeNode(arr[ind], makeTree(arr[:ind]), makeTree(arr[ind+1:]))
+
+a = range(9)
+tree = makeTree(a)
+print tree.data
+print tree.left.data
+print tree.right.data
